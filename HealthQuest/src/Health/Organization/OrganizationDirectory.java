833c566828bacb33b8488458771321430b67d281
Hanagojiv <hanagoji.v@northeastern.edu>
2022-12-09 01:57:16 -0500

    Organization and OrganizationDirectory class

---
+++ HealthQuest/src/Health/Organization/OrganizationDirectory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Health.Organization;
+
+import Health.Organization.Organization.Type;
+import java.util.ArrayList;
+
+
+public class OrganizationDirectory {
+    
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        return organization;
+    }
+}
